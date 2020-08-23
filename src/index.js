a5eb8870965dabadf967e0ed9ae2d5d5865d4701
Felipe Lucio <felipeluc@gmail.com>
2020-08-23 13:59:54 -0300

    dist folder changed to docs (github pages)

--- src/index.js
+++ src/index.js
@@ -10,36 +10,39 @@ const sketches = {
     line
 };
 
-const container = document.getElementById('container');
-var p5_instance = null;
-
-function selectSketch(name) {
-    if (p5_instance) {
-        console.log('removing');
-        p5_instance.remove();
-        p5_instance = null;
+window.addEventListener('load', ev => {
+    
+    const container = document.getElementById('container');
+    var p5_instance = null;
+    
+    function selectSketch(name) {
+        if (p5_instance) {
+            console.log('removing');
+            p5_instance.remove();
+            p5_instance = null;
+        }
+    
+        if (sketches.hasOwnProperty(name)) {
+            p5_instance = new p5(sketches[name]);
+        }
     }
+    
+    // default sketch
+    selectSketch('hello');
+    
+    // MENU SKETCH SELECT
+    const menu_select = document.getElementById('sketch-select');
+    Object.keys(sketches).forEach(key => {
+        let option = document.createElement('option');
+        option.value = key;
+        option.innerText = key;
+        menu_select.add(option);
+    });
+    menu_select.appendChild
+    menu_select.addEventListener('change', (ev) => {
+        ev.preventDefault();
+        selectSketch(ev.target.value);
+    })
+    
 
-    if (sketches.hasOwnProperty(name)) {
-        p5_instance = new p5(sketches[name]);
-    }
-}
-
-// default sketch
-selectSketch('hello');
-
-// MENU SKETCH SELECT
-const menu_select = document.getElementById('sketch-select');
-Object.keys(sketches).forEach(key => {
-    let option = document.createElement('option');
-    option.value = key;
-    option.innerText = key;
-    menu_select.add(option);
-});
-menu_select.appendChild
-menu_select.addEventListener('change', (ev) => {
-    ev.preventDefault();
-    selectSketch(ev.target.value);
-})
-
-
+})    
